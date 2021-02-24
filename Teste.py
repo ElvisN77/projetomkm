11705dc0aa801ea0c27cb2141ed6d42e75f26a9f
Elvis <elvisnevis777@gmail.com>
2021-02-23 21:16:27 -0300

    Programa simples cálculo média em python

---
+++ Teste.py
@@ -0,0 +1,15 @@
+# Média
+
+nota1 = input("Digite a primeira nota:")
+nota2 = input("Digite a segunda nota:")
+nota3 = input("Digite a terceira nota:")
+nota4 = input("Digite a quarta nota:")
+
+n1 = float(nota1)
+n2 = float(nota2)
+n3 = float(nota3)
+n4 = float(nota4)
+
+media = (n1+n2+n3+n4)/4
+
+print("A média aritmética é", media)
